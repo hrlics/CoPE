c5449aace2be9c160819cd69e04881207b1c7207
Haoran Li <hli43@gh-login01.delta.ncsa.illinois.edu>
2026-02-05 22:21:07 -0600

    add training

---
+++ train/setup_env.sh
@@ -0,0 +1,5 @@
+conda activate cope_train
+pip install torch==2.4.1
+pip install -r requirements.txt
+pip install flash-attn==2.6.1 --no-build-isolation
+pip install git+https://github.com/Dao-AILab/flash-attention.git@9356a1c0389660d7e231ff3163c1ac17d9e3824a#subdirectory=csrc/rotary
